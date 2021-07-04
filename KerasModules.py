64c0c31ab9594044e85a3596a8de73ebdf8cc73a
Nacho Villaluenga <nachovillaluenga@gmail.com>
2021-07-04 16:51:01 -0300

    feat: continue adding building blocks

--- KerasModules.py
+++ KerasModules.py
@@ -16,3 +16,58 @@ class HighwayNet(tf.keras.layers.Layer):
         t = self.T(inputs)
         outputs = h * t + inputs * (1. - t)
         return outputs
+
+
+class HighwayCNet(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, dilation_rate, dropout_rate):
+        super(HighwayCNet, self).__init__()
+        self.conv1d = tf.keras.layers.Conv1D(
+            filters=2*filters,
+            kernel_size=kernel_size,
+            dilation_rate=dilation_rate,
+            kernel_initializer=tf.keras.initializers.variance_scaling,
+            use_bias=True)
+        self.normalize = tf.keras.layers.LayerNormalization()
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+    def call(self, inputs, is_training):
+        _inputs = inputs
+        tensor = self.conv1d(inputs)
+        H1, H2 = tf.split(tensor, 2, axis=-1)
+        H1 = self.normalize(H1)
+        H2 = self.normalize(H2)
+        H1 = tf.nn.sigmoid(H1, "gate")
+        tensor = H1*H2 + (1.-H1)*_inputs
+        tensor = self.dropout(tensor, training=is_training)
+
+        return tensor
+
+
+class HighwayCNet_experimental(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, dilation_rate, dropout_rate):
+        super(HighwayCNet_experimental, self).__init__()
+        self.conv1d = tf.keras.layers.Conv1D(
+            filters=2*filters,
+            kernel_size=kernel_size,
+            dilation_rate=dilation_rate,
+            kernel_initializer=tf.keras.initializers.variance_scaling,
+            use_bias=True)
+        self.normalize = tf.keras.layers.LayerNormalization()
+        self.T = tf.keras.layers.Dense(
+            units=filters,
+            activation="sigmoid",
+            bias_initializer=tf.keras.initializers.Constant(-3.0)
+        )
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+    def call(self, inputs, is_training):
+        _inputs = inputs
+        tensor = self.conv1d(inputs)
+        H1, H2 = tf.split(tensor, 2, axis=-1)
+        H1 = self.normalize(H1)
+        H2 = self.normalize(H2)
+        H1 = self.T(H1)
+        tensor = H1*H2 + (1.-H1)*_inputs
+        tensor = self.dropout(tensor, training=is_training)
+
+        return tensor
--- KerasModulesTest.py
+++ KerasModulesTest.py
@@ -1,6 +1,6 @@
 import unittest
 import tensorflow as tf
-from KerasModules import HighwayNet
+from KerasModules import HighwayNet, HighwayCNet, HighwayCNet_experimental
 
 
 class KerasModuleTest(unittest.TestCase):
@@ -15,6 +15,20 @@ class KerasModuleTest(unittest.TestCase):
         hwNetLayer = HighwayNet(10)
         input = tf.constant(1., shape=(2, 10))
         output = hwNetLayer(input)
+        # print(output)
+        assert input.shape == output.shape
+
+    def testHighwayCNet(self):
+        hwCNetLayer = HighwayCNet(10, 1, 1, 0)
+        input = tf.constant(1., shape=(1, 2, 10))
+        output = hwCNetLayer(input, False)
+        assert input.shape == output.shape
+
+    def testHighwayCNet_exp(self):
+        hwCNetLayer = HighwayCNet_experimental(10, 1, 1, 0)
+        input = tf.constant(1., shape=(1, 2, 10))
+        output = hwCNetLayer(input, False)
+        print(output)
         assert input.shape == output.shape
 
 
